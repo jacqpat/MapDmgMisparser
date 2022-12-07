5e6d8c0ddb84f8781cd77867bef2a1858da5ee1e
jacqpat <patrickjacques@laposte.net>
2022-12-07 11:55:41 +0100

    new functions

--- home.sh
+++ home.sh
@@ -1,1 +1,55 @@
 #!/bin/bash
+
+get_misalignment(dir){
+
+}
+
+find_mapDamage(dir){
+
+}
+
+check_dirname(dir){
+    dirname="$(basename -- $dir)"
+    if [ "$dirname" == *mapDamage ]
+    then
+        get_misincorporations($dir);
+    else
+        find_mapDamage($dir);
+    fi
+}
+
+check_filename(fil){
+    filname="$(basename -- $fil)"
+    if [ "$filname" == "misincorporation.txt" ]
+    then
+        txt2csv($fil);
+    else
+        echo "$arg is not a misincorporation file"
+    fi
+
+}
+
+check_paths(arglist){
+    for arg in $arglist
+    do
+        if [[ -d $arg ]]
+        then
+            echo "$arg is a directory";
+            check_dirname;
+        elif [[ -f $arg ]]
+        then
+            echo "$arg is a file";
+        else
+            echo "$arg is not valid";
+            exit 1;
+        fi
+    done
+}
+
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied";
+    exit 1;
+fi
+arglist=( "$@" )
+check_paths($arglist)
